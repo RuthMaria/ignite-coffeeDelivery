be9cb3663f30b4daabc0a0d150c0d48cf6b6410c
RuthMaria <ruth-15rms@hotmail.com>
2022-11-12 21:56:49 -0300

    created card and refactor css

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,10 +1,8 @@
 import styled from 'styled-components';
 
 export const LayoutContainer = styled.div`
-  max-width: 90rem;
   min-height: calc(100vh - 6.5rem);
-  margin: 2rem auto;
-  padding: 0 10rem;
+  margin: 2rem 0;
   display: flex;
   flex-direction: column;
 `;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,68 @@
 import React from 'react';
+import { Plus } from 'phosphor-react';
 import { Banner } from '../components/Banner';
+import {
+  Title,
+  Card,
+  Tag,
+  CoffeName,
+  Price,
+  Description,
+  Group,
+  Section,
+  Image,
+  Quantity,
+  Content,
+} from './styles';
+import expresso from '../../assets/expresso.svg';
+import shoppingCartSimple from '../../assets/shoppingCartSimple.svg';
+import less from '../../assets/less.svg';
+import { defaultTheme } from '../../styles/themes/default';
 
 export const Home: React.FC = () => {
-  return <Banner />;
+  return (
+    <div>
+      <Banner />
+
+      <Content>
+        <Title>Nossos cafés</Title>
+
+        <Section>
+          <Card>
+            <Image>
+              <div>
+                <img src={expresso} alt="" />
+              </div>
+            </Image>
+
+            <Tag>TRADICIONAL</Tag>
+
+            <CoffeName>Expresso Tadicional</CoffeName>
+            <Description>
+              O tradicional café feito com água quente e grãos moídos
+            </Description>
+
+            <Group>
+              <Price>
+                <span>R$</span>
+                <span>9,90</span>
+              </Price>
+
+              <Quantity>
+                <div>
+                  <img src={less} alt="" />
+                  <span>1</span>
+                  <Plus color={defaultTheme['purple-dark']} />
+                </div>
+
+                <div>
+                  <img src={shoppingCartSimple} alt="" />
+                </div>
+              </Quantity>
+            </Group>
+          </Card>
+        </Section>
+      </Content>
+    </div>
+  );
 };
