87db1d99ec6c00b3d7d5de5739f36bc33c87b8e6
RuthMaria <ruth-15rms@hotmail.com>
2022-12-12 21:54:21 -0300

    feat: add new roles

--- src/pages/Delivery/Delivery.tsx
+++ src/pages/Delivery/Delivery.tsx
@@ -16,13 +16,19 @@ enum PaymentMethods {
 }
 
 const addressFormValidationSchema = zod.object({
-  cep: zod.string().min(1, 'Informe o CEP'),
+  cep: zod
+    .string()
+    .min(1, 'Informe o CEP')
+    .max(9, 'Informe no máximo 8 números'),
   street: zod.string().min(1, 'Informe a rua'),
   number: zod.string().min(1, 'Informe o número'),
   complement: zod.string().optional(),
   district: zod.string().min(1, 'Informe o bairro'),
   city: zod.string().min(1, 'Informe a cidade'),
-  uf: zod.string().min(1, 'Informe o UF'),
+  uf: zod
+    .string()
+    .min(1, 'Informe o UF')
+    .max(2, 'Informe no máximo 2 caracteres'),
   paymentMethod: zod.nativeEnum(PaymentMethods, {
     errorMap: () => {
       return { message: 'Informe o método de pagamento' };
